6c28752b075594d8f45a260221abda223f87e3f6
herokeyvn <herokey2018@gmail.com>
2026-03-16 15:02:11 +0700

    #

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,3 @@
 "use strict";
 
-// Reserved for domain models. Kept as an explicit MVC layer entrypoint.
 module.exports = {};
--- src/views/index.js
+++ src/views/index.js
@@ -1,4 +1,3 @@
 "use strict";
 
-// Reserved for response/view mappers. Kept as an explicit MVC layer entrypoint.
 module.exports = {};
